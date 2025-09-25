287db942fcb17b2f7ddf76523fe7eaf41f739b9b
Joshua Blais <josh@joshblais.com>
2025-09-25 10:17:33 -0600

    flake build

--- flake.nix
+++ flake.nix
@@ -1,15 +1,24 @@
 {
-  description = "A very basic flake";
+  description = "API Key generation tooling written in Go";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.${system}.default = pkgs.buildGoModule {
+        pname = "keygen";
+        version = "1.0.0";
+        src = ./.;
+        vendorHash = null;
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+        # Rename binary from go-secrets to secrets
+        postInstall = ''
+          mv $out/bin/go-api-key $out/bin/keygen
+        '';
+      };
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ pkgs.go pkgs.age ];
+      };
+    };
 }
